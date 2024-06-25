7a2e8d4dad5cf844fe5b2f762d1f05680fd23b1a
Aymeric Soubrouillard <asoubrouillard@gmail.com>
2024-06-25 10:08:28 +0200

    Ajout du mock sur le test checkout

--- ProjetFormation.Tests/tuto1/doublure/OrderServiceTest.cs
+++ ProjetFormation.Tests/tuto1/doublure/OrderServiceTest.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 public class OrderServiceTests
 {
     [Test]
-    public void ProcessOrder_ShouldUpdateOrderStatusToProcessed()
+    public void ProcessOrder_ShouldUpdateOrderStatusToProcessedStub()
     {
         // Given
         var stubOrderRepository = new StubOrderRepository();
@@ -19,6 +19,35 @@ public class OrderServiceTests
 
         // Then
         Assert.AreEqual("Processed", order.Status);
+    }
+
+        [Test]
+    public void ProcessOrder_ShouldUpdateOrderStatusToProcessedMock()
+    {
+        // Given
+        var mockOrderRepository = new Mock<IOrderRepository>();
+        var order = new Order(1,"New","John");
+        mockOrderRepository.Setup(repo => repo.GetOrderById(1)).Returns(order);
+        var orderService = new OrderService(mockOrderRepository.Object);
+        // When
+        orderService.ProcessOrder(1);
+        // Then
+        Assert.AreEqual("Processed", order.Status);
+        mockOrderRepository.Verify(repo => repo.GetOrderById(1), Times.Once);
+    }
+
+    
+    public void ProcessOrder_ShouldSaveInRepository()
+    {
+        // Given
+        var mockOrderRepository = new Mock<IOrderRepository>();
+        var order = new Order(1,"New","John");
+        mockOrderRepository.Setup(repo => repo.GetOrderById(1)).Returns(order);
+        var orderService = new OrderService(mockOrderRepository.Object);
+        // When
+        orderService.ProcessOrder(1);
+        // Then
+        mockOrderRepository.Verify(repo => repo.Save(order), Times.Once);
     }
     
 }
